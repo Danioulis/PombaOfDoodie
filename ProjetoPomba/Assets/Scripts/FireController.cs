22a7eff9fe1080fa1703c4b2be8f4c7e5c84484b
Daniel Bustorff Silva <dbustorff@hotmail.com>
2016-05-12 17:15:11 -0300

    Modelo pomba alterado

--- ProjetoPomba/Assets/Scripts/FireController.cs
+++ ProjetoPomba/Assets/Scripts/FireController.cs
@@ -20,5 +20,10 @@ public class FireController : MonoBehaviour
         {
             Destroy(gameObject);
         }
+		if (other.gameObject.CompareTag ("Enemy")) 
+		{
+			Destroy(gameObject);
+			Destroy(other.gameObject);
+		}
     }
 }
